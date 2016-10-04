f60ba88630eba4bcd47b5a878985387efd124cae
shun <shun@getto.systems>
2016-10-04 15:11:33 +0000

    version-dump

--- version-dump.sh
+++ version-dump.sh
@@ -8,6 +8,20 @@ if [ -z "$mode" ]; then
 fi
 
 version_file=version.txt
+current_version=$(cat $version_file)
+current_tag=$(git tag | tail -1)
+
+if [ "v$current_version" != "$current_tag" ]; then
+  read -p "file: '$current_version', tag: '$current_tag'. continue? [Y/n] " confirm
+  case $confirm in
+    Y*|y*)
+      ;;
+    *)
+      exit 1
+      ;;
+  esac
+fi
+
 version_build_next "$mode" $(cat $version_file)
 
 read -p "dump version: $version. OK? [Y/n] " confirm
