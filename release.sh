344a21c0023209f087dc606116c4ca1592ba50c5
shun <shun@getto.systems>
2016-10-27 03:52:51 +0000

    fix release script

--- release.sh
+++ release.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-git up && git tag v$(cat version.txt) && git push origin --tags
+
+release_version_prefix="version dump: "
+current_version=$(git log --format="%s" --grep="$release_version_prefix" | head -1)
+current_version=${current_version#$release_version_prefix}
+
+read -p "release: '$current_version'. continue? [Y/n] " confirm
+case $confirm in
+  Y*|y*)
+    ;;
+  *)
+    exit 1
+    ;;
+esac
+
+git up && git tag v$current_version && git push origin --tags
